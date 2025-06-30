6dda64646cc1bfea7d2cdc2f74742fe9e667b94e
Bin Miao <bin.miao@intel.com>
2025-06-30 17:38:18 +0800

    Keep ’com.ms.internal.nhwc‘ domain opset version aligned with default ORT (#239)
    This PR fixes the issues that some operators such as
    ’BatchNormalization‘, ’InstanceNormalization‘ and ’averagePool2d‘ fail
    to run on ORT WebGPU EP.

--- services/webnn/ort/ort_model_editor.h
+++ services/webnn/ort/ort_model_editor.h
@@ -36,7 +36,7 @@ constexpr int32_t kOrtOpsetVersion = 21;
 constexpr int32_t kEPContextOpsetVersion = 1;
 constexpr int32_t kMSNchwcDomainOpsetVersion = 1;
 constexpr int32_t kMSDmlDomainOpsetVersion = 1;
-constexpr int32_t kMSInternalNhwcDomainOpsetVersion = 1;
+constexpr int32_t kMSInternalNhwcDomainOpsetVersion = kOrtOpsetVersion;
 
 // Define the minimum size(in bytes) to use external data.
 constexpr size_t kMinExternalDataSize = 128;
